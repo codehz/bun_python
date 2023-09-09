5ad6b40b393bab18ae0ddf49482407e704a2da4c
codehz <git@hertz.moe>
2023-09-09 23:33:41 +0800

    add deno bench

---
+++ examples/bench.deno.js
@@ -0,0 +1,18 @@
+import { python } from "https://deno.land/x/python/mod.ts";
+
+const count = 100_000_000;
+
+console.time("js for-loop");
+for (let i = 0; i < count; i++) {}
+console.timeEnd("js for-loop");
+
+console.time("python for-loop in js");
+for (const _ of python.builtins.range(0, count)) {
+}
+console.timeEnd("python for-loop in js");
+
+console.log("python for-loop in cProfile");
+python.import("cProfile").run(`
+for i in range(1, ${count}):
+  pass
+`);
